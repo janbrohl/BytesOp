762d0675f7ee4a9d7e952ddb7a282683a760377e
Jan Brohl <janbrohl@t-online.de>
2016-04-03 22:55:29 +0200

    edit manifest to include readmes

--- build.bat
+++ build.bat
@@ -1,7 +1,7 @@
 del /s /q dist build BytesOp.egg-info
 c:\python27\scripts\autopep8.exe --in-place setup.py
 py -2 setup.py sdist
-py -2 setup.py build bdist --plat-name=win32 bdist_wininst bdist_msi
-py -2 setup.py build bdist --plat-name=win-amd64 bdist_wininst bdist_msi
+py -2 setup.py build bdist --plat-name=win32 bdist_wininst
+py -2 setup.py build bdist --plat-name=win-amd64 bdist_wininst
 py -2 setup.py bdist_wheel --plat-name=win32
 py -2 setup.py bdist_wheel --plat-name=win-amd64
--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ setup(
     author="Jan Brohl",
     author_email="janbrohl@t-online.de",
     url="https://github.com/janbrohl/BytesOp",
-    version="0.2",
+    version="0.3",
     description="Module for bitwise operations on bytestrings.",
     ext_modules=[module]
 )
