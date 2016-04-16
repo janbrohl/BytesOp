c731d083af610db3594415e09b875e3fca403d5e
Jan Brohl <janbrohl@t-online.de>
2016-04-16 14:24:49 +0200

    bump version

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ setup(
     author="Jan Brohl",
     author_email="janbrohl@t-online.de",
     url="https://github.com/janbrohl/BytesOp",
-    version="0.8",
-    description="Module for bitwise operations on bytestrings.",
+    version="0.10",
+    description="Bitwise operations on bytestrings.",
     ext_modules=[module],
     test_suite="test_bytesop.TestOps",
     classifiers=[
