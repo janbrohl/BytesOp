c0cd773dd5c3086da96cf2c1fcc817b2637e4798
Jan Brohl <janbrohl@t-online.de>
2016-04-16 14:25:16 +0200

    streamline build (a bit)

--- build.bat
+++ build.bat
@@ -3,9 +3,7 @@ py -2 -m autopep8 --in-place setup.py test_bytesop.py
 py -2 setup.py test
 py -3 setup.py test
 py -2 setup.py sdist
-py -2 setup.py bdist_wininst --plat-name=win32  
-py -2 setup.py bdist_wheel --plat-name=win32
+py -2 setup.py bdist_wininst bdist_wheel --plat-name=win32  
 py -2 setup.py bdist_wheel --plat-name=win-amd64
-py -3 setup.py bdist_wininst --plat-name=win32
-py -3 setup.py bdist_wheel --plat-name=win32
+py -3 setup.py bdist_wininst bdist_wheel --plat-name=win32
 py -3 setup.py bdist_wheel --plat-name=win-amd64
